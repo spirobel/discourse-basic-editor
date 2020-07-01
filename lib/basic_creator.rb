1d5a5957fef300c6b48c7148bac515af6edec31c
spirobel <spirobel@protonmail.com>
2020-07-01 16:26:00 +0800

    FIX camping cooker: inheritable class variable

--- lib/basic_creator.rb
+++ lib/basic_creator.rb
@@ -1,8 +1,9 @@
 class BasicCreator
+  class_attribute :template
   def initialize(jraw, opts)
     filename = File.basename(self.method(:create).source_location[0] , ".rb")
     dirname = File.dirname(self.method(:create).source_location[0])
-    @@template ||= File.read(dirname + "/"+filename+ ".html.erb")
+    self.template ||= File.read(dirname + "/"+filename+ ".html.erb")
     @jraw = jraw
     @opts = opts
   end
